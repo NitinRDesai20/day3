159eb64f92220b892ed22911afa3abc3bf6ae547
NitinRDesai20 <148466355+NitinRDesai20@users.noreply.github.com>
2023-10-28 23:30:24 +0530

    Add files via upload

---
+++ Day 3/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Functional from './Components/Functional';
+import Arrow from './Components/Arrow';
+import Cla from './Components/Cla';
+
+const App = () => {
+  return (
+      <div>
+        <Functional/>
+        <Arrow/>
+        <Cla/>
+      </div>
+  );
+};
+
+export default App;
---
+++ Day 3/src/Components/Arrow.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Arrow.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+const Arrow = () => {
+  return (
+    <>
+      <h1 id="header" className="text-center">This is Arrow Functional component</h1>
+      <p>Lorem ipsum dolor sit amet?</p>
+      <input type="text" />
+    </>
+  );
+};
+
+export default Arrow;
